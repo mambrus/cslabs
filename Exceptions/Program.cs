43dfe8aee41367b62bf6d3d85f199a9c0648ed78
Michael Ambrus <michael.ambrus@medius.se>
2017-03-14 20:41:28 +0100

    Exception: Tutorial code added
    Somewhat reorganized

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace Exceptions
 {
-  class Program
+
+  public class TempIsZeroException : Exception
   {
-    static void Main(string[] args)
+    public TempIsZeroException(string message) : base(message)
     {
+    }
+  }
+
+  public class Temperature
+  {
+    int temperature = 0;
+
+    public void showTemp()
+    {
+      if (temperature == 0)
+      {
+        throw (new TempIsZeroException("Zero Temperature found"));
+      }
+      else
+      {
+        Console.WriteLine("Temperature: {0}", temperature);
+      }
+    }
+
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        Temperature temp = new Temperature();
+        try
+        {
+          temp.showTemp();
+        }
+        catch (TempIsZeroException e)
+        {
+          Console.WriteLine("TempIsZeroException: {0}", e.Message);
+        }
+        Console.ReadKey();
+      }
     }
   }
 }
