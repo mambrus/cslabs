c53319ffdb3aca01b8d9a01b109bdd23b65670a1
Michael Ambrus <michael.ambrus@medius.se>
2017-03-27 00:08:29 +0200

    Reflection: refactored

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,56 +1,15 @@
 ﻿using System;
 using System.Reflection;
 
+using Reflection.Attributes;
+
 namespace Reflection
 {
-  //a custom attribute BugFix to be
-  //assigned to a class and its members
-  [AttributeUsage(AttributeTargets.Class |
-                  AttributeTargets.Constructor |
-                  AttributeTargets.Field |
-                  AttributeTargets.Method |
-                  AttributeTargets.Property,
-    AllowMultiple = true)]
 
-  public class DeBugInfo : System.Attribute
-  {
-    private int bugNo;
-    private string developer;
-    private string lastReview;
-    public string message;
+  #region Class rectangle
 
-    public DeBugInfo(int bg, string dev, string d)
-    {
-      this.bugNo = bg;
-      this.developer = dev;
-      this.lastReview = d;
-    }
-
-    public int BugNo
-    {
-      get { return bugNo; }
-    }
-
-    public string Developer
-    {
-      get { return developer; }
-    }
-
-    public string LastReview
-    {
-      get { return lastReview; }
-    }
-
-    public string Message
-    {
-      get { return message; }
-      set { message = value; }
-    }
-  }
-
-  [DeBugInfo(45, "Zara Ali", "12/8/2012", Message = "Return type mismatch")]
-  [DeBugInfo(49, "Nuha Ali", "10/10/2012", Message = "Unused variable")]
-
+  [BugTag(45, "Zara Ali", "12/8/2012", Message = "Return type mismatch")]
+  [BugTag(49, "Nuha Ali", "10/10/2012", Message = "Unused variable")]
   class Rectangle
   {
     //member variables
@@ -63,59 +22,83 @@ namespace Reflection
       width = w;
     }
 
-    [DeBugInfo(55, "Zara Ali", "19/10/2012", Message = "Return type mismatch")]
+    [BugTag(55, "Zara Ali", "19/10/2012", Message = "Return type mismatch")]
     public double GetArea()
     {
       return length * width;
     }
 
-    [DeBugInfo(56, "Zara Ali", "19/10/2012")]
+    [BugTag(56, "Zara Ali", "19/10/2012")]
     public void Display()
     {
       Console.WriteLine("Length: {0}", length);
       Console.WriteLine("Width: {0}", width);
       Console.WriteLine("Area: {0}", GetArea());
     }
-  } //end class Rectangle
+  }
+
+  #endregion
+
+  #region class Program
 
   class Program
   {
+    static void PrintBug(ref BugTag dbi)
+    {
+      Console.WriteLine("Bug no: {0}", dbi.BugNo);
+      Console.WriteLine("Developer: {0}", dbi.Developer);
+      Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+      Console.WriteLine("Remarks: {0}", dbi.Message);
+      Console.WriteLine("........................");
+    }
+
     static void Main(string[] args)
     {
-      Rectangle r = new Rectangle(4.5, 7.5);
-      r.Display();
-      Type type = typeof(Rectangle);
+      Rectangle rectangle = new Rectangle(4.5, 7.5);
+      rectangle.Display();
+      Console.WriteLine("========================");
+      Type classInfo = typeof(Rectangle);
 
-      //iterating through the attribtues of the Rectangle class
-      foreach (Object attributes in type.GetCustomAttributes(false))
+      #region iterating through the attribtues of the Rectangle class
+
+      Console.WriteLine();
+      Console.WriteLine("Print bugs tagged for Rectangle class");
+      Console.WriteLine("=====================================");
+      foreach (Object classAttribute in classInfo.GetCustomAttributes(false))
       {
-        DeBugInfo dbi = (DeBugInfo) attributes;
+        BugTag dbi = (BugTag) classAttribute;
         if (null != dbi)
         {
-          Console.WriteLine("Bug no: {0}", dbi.BugNo);
-          Console.WriteLine("Developer: {0}", dbi.Developer);
-          Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
-          Console.WriteLine("Remarks: {0}", dbi.Message);
+          PrintBug(ref dbi);
         }
       }
 
-      //iterating through the method attribtues
-      foreach (MethodInfo m in type.GetMethods())
+      #endregion
+
+      #region  iterating through the method attribtues
+
+      Console.WriteLine();
+      Console.WriteLine("Print bugs tagged for each method");
+      Console.WriteLine("=====================================");
+      foreach (MethodInfo methodInfo in classInfo.GetMethods())
       {
-        foreach (Attribute a in m.GetCustomAttributes(true))
+        Console.WriteLine("Method: {0}", methodInfo.Name);
+        Console.WriteLine("........................");
+        foreach (Attribute methodAttribute in methodInfo.GetCustomAttributes(true))
         {
-          DeBugInfo dbi = (DeBugInfo) a;
+          BugTag dbi = (BugTag) methodAttribute;
           if (null != dbi)
           {
-            Console.WriteLine("Bug no: {0}, for Method: {1}", dbi.BugNo, m.Name);
-            Console.WriteLine("Developer: {0}", dbi.Developer);
-            Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
-            Console.WriteLine("Remarks: {0}", dbi.Message);
+            PrintBug(ref dbi);
           }
         }
       }
 
+      #endregion
+
       Console.ReadLine();
     }
   }
+
+  #endregion
 }
