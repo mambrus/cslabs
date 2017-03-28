718b5539a4cdebbdfa2d877fef5ec75db9a39f17
Michael Ambrus <michael.ambrus@medius.se>
2017-03-28 17:13:31 +0200

    SimpleActors: Make build-chain shut-up

---
+++ SimpleActors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleActors
+{
+  using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("You're not actually supposed to run this. Use NUnit executive instead..");
+      Console.ReadKey();
+    }
+  }
+}
