9106b1c6ed9e7099d70d9d127282c95fe56576ec
Michael Ambrus <michael.ambrus@medius.se>
2017-03-28 15:11:56 +0200

    Stumbling start with NUnit-test setup...

---
+++ SimpleActors/AccountSpecs.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleActors
+{
+  using System;
+  using NUnit.Framework;
+
+  [TestFixture]
+  class AccountSpecs
+  {
+    [Test]
+    static void Chargeing_an_account_should_adjust_the_balence()
+    {
+      Console.WriteLine("Hello world");
+      Console.ReadKey();
+    }
+  }
+}
