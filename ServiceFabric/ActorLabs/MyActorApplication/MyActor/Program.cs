ea070360b0823df08514233a593fa63954ef796b
Michael Ambrus <michael.ambrus@medius.se>
2017-03-30 20:06:34 +0200

    Start interfacing...

--- ServiceFabric/ActorLabs/MyActorApplication/MyActor/Program.cs
+++ ServiceFabric/ActorLabs/MyActorApplication/MyActor/Program.cs
@@ -3,7 +3,10 @@ using System.Diagnostics;
 using System.Fabric;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
 using Microsoft.ServiceFabric.Actors.Runtime;
+using MyActor.Interfaces;
 
 namespace MyActor
 {
@@ -23,6 +26,24 @@ namespace MyActor
 
         ActorRuntime.RegisterActorAsync<MyActor>(
            (context, actorType) => new ActorService(context, actorType)).GetAwaiter().GetResult();
+
+        Console.WriteLine("Actor runtime registration completed");
+
+        // Speak with actor a few times. I.e. send messages.
+        // Create a randomly distributed actor ID
+        ActorId actorId = ActorId.CreateRandom();
+
+        // This only creates a proxy object, it does not activate an actor or invoke any methods yet.
+        IMyActor myActor = ActorProxy.Create<IMyActor>(actorId, new Uri("fabric:/MyApp/MyActorService"));
+
+        // Create the token source.
+        CancellationTokenSource source = new CancellationTokenSource();
+        CancellationToken cts = source.Token;
+
+        // This will invoke a method on the actor. If an actor with the given ID does not exist, it will
+        // be activated by this method call.
+        myActor.SetCountAsync(1, cts);
+
 
         Thread.Sleep(Timeout.Infinite);
       }
