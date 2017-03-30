50befde8e3ec77306a14f58adf09c1bea95e2614
Michael Ambrus <michael.ambrus@medius.se>
2017-03-30 20:26:56 +0200

    Threading: Waiting for threads to complete

---
+++ Threading/CancellationToken/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationToken
+{
+  class Program
+  {
+
+    public static void Main()
+    {
+      // Define the cancellation token.
+      CancellationTokenSource source = new CancellationTokenSource();
+      System.Threading.CancellationToken token = source.Token;
+
+      Random rnd = new Random();
+      Object lockObj = new Object();
+
+      List<Task<int[]>> tasks = new List<Task<int[]>>();
+      TaskFactory factory = new TaskFactory(token);
+      for (int taskCtr = 0; taskCtr <= 10; taskCtr++)
+      {
+        int iteration = taskCtr + 1;
+        tasks.Add(factory.StartNew(() =>
+          {
+            int[] values = new int[10];
+            for (int ctr = 1; ctr <= 10; ctr++)
+            {
+              int value;
+              lock (lockObj)
+              {
+                value = rnd.Next(0, 101);
+              }
+              if (value == 0)
+              {
+                source.Cancel();
+                Console.WriteLine("Cancelling at task {0}", iteration);
+                break;
+              }
+
+              values[ctr - 1] = value;
+            }
+
+            return values;
+          },
+          token));
+
+      }
+
+      try
+      {
+        Task<double> fTask = factory.ContinueWhenAll(tasks.ToArray(),
+          (results) =>
+          {
+            Console.WriteLine("Calculating overall mean...");
+            long sum = 0;
+            int n = 0;
+            foreach (var t in results)
+            {
+              foreach (var r in t.Result)
+              {
+                sum += r;
+                n++;
+              }
+            }
+
+            return sum / (double) n;
+          },
+          token);
+        Console.WriteLine("The mean is {0}.", fTask.Result);
+      }
+      catch (AggregateException ae)
+      {
+        foreach (Exception e in ae.InnerExceptions)
+        {
+          if (e is TaskCanceledException)
+            Console.WriteLine("Unable to compute mean: {0}",
+              ((TaskCanceledException) e).Message);
+          else
+            Console.WriteLine("Exception: " + e.GetType().Name);
+        }
+      }
+      finally
+      {
+        source.Dispose();
+      }
+
+      Console.WriteLine("Hello Windows ridiculas tasker");
+      Console.ReadKey();
+    }
+  }
+}
